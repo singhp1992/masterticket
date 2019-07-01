a407b9a6bbe6167856a30d4a6aa43fe0d142f0d0
Preeti <singhp1992@gmail.com>
2019-07-01 19:44:54 +0200

    user is notified when tickets are sold out

--- masterticket.py
+++ masterticket.py
@@ -3,37 +3,34 @@ TICKET_PRICE = 10
 tickets_remaining = 100 
 
 # code runs until no more tickets
+while tickets_remaining:
+    #output how many tickets are remaining using the tickets_remaining variable
+    print("There are {} tickets left!".format(tickets_remaining))
 
+    # gather user name and assign to a variable
+    name = raw_input("What is your name? ").capitalize()
 
+    # prompt the user by name and ask how many tickets they would like
+    number_tickets = raw_input("How many tickets would you like, {}?  ".format(name))
+    number_tickets = int(number_tickets)
 
+    # calculate the price (number of tickets x price) and assign to a variable
+    amount = number_tickets * TICKET_PRICE
 
-#output how many tickets are remaining using the tickets_remaining variable
-print("There are {} tickets left!".format(tickets_remaining))
+    # output the price to the screen
+    print("Total: {}".format(amount))
 
-# gather user name and assign to a variable
-name = raw_input("What is your name? ").capitalize()
+    # prompt continue y/n
+    continue_yn = raw_input("Do you want to proceed? Y/N  ")
 
-# prompt the user by name and ask how many tickets they would like
-number_tickets = raw_input("How many tickets would you like, {}?  ".format(name))
-number_tickets = int(number_tickets)
-
-# calculate the price (number of tickets x price) and assign to a variable
-amount = number_tickets * TICKET_PRICE
-
-# output the price to the screen
-print("Total: {}".format(amount))
-
-# prompt continue y/n
-continue_yn = raw_input("Do you want to proceed? Y/N  ")
-
-# YES - print "Sold!"
-if continue_yn.lower() == "y": 
-    print("Sold!")
-    # decrement the tickets remaining by the number of tickets purchased
-    tickets_remaining -= number_tickets
-# NO - thank you, name
-else: 
-    print("Thank you for your time, {}".format(name))
-
+    # YES - print "Sold!"
+    if continue_yn.lower() == "y": 
+        print("Sold!")
+        # decrement the tickets remaining by the number of tickets purchased
+        tickets_remaining -= number_tickets
+    # NO - thank you, name
+    else: 
+        print("Thank you for your time, {}".format(name))
 
 # tell user tickets are gone
+print("Sorry the tickets are sold out! :(")
