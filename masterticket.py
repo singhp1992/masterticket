c11f89a86f3dec08d4dabafd185f77d1d27b8769
Preeti <singhp1992@gmail.com>
2019-07-01 19:05:58 +0200

    adding comments as a lead

--- masterticket.py
+++ masterticket.py
@@ -2,6 +2,14 @@ TICKET_PRICE = 10
 
 tickets_remaining = 100 
 
+#output how many tickets are remaining using the tickets_remaining variable
 print("There are {} tickets left!".format(tickets_remaining))
 
+# gather user name and assign to a variable
 name = input("What is your name?  ")
+
+# prompt the user by name and ask how many tickets they would like
+
+# calculate the price (number of tickets x price) and assign to a variable
+
+# output the price to the screen
